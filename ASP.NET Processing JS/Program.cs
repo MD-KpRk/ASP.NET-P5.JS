2ef04dc71d1ca62ad86589833f06c50f5ad6eac5
MD-KpRk <griferad@gmail.com>
2022-01-31 22:58:34 +0300

    Тестирование передачи управления из одного middleware в другой

--- ASP.NET Processing JS/Program.cs
+++ ASP.NET Processing JS/Program.cs
@@ -2,14 +2,19 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 WebApplication app = builder.Build();
 
 
-app.Run( async (context) =>
+app.Use( async (context,next) =>
 {
     if(context.Request.Path == "/g")
          context.Response.Redirect("https://google.com");
     if (context.Request.Path == "/y")
         context.Response.Redirect("https://yandex.ru");
-    else await context.Response.WriteAsync(context.Request.Path);
+    else await next.Invoke();
 
+});
+
+app.Run(async (context) =>
+{
+    await context.Response.WriteAsync(context.Request.Path);
 });
 
 
