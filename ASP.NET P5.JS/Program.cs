7ce4b5c8913d294ae5bef2ba235e771847b42ce5
MD-KpRk <griferad@gmail.com>
2022-02-01 03:00:14 +0300

    Переделал проект под Razor Pages

--- ASP.NET P5.JS/Program.cs
+++ ASP.NET P5.JS/Program.cs
@@ -1,28 +1,31 @@
-var builder = WebApplication.CreateBuilder(
-    new WebApplicationOptions { WebRootPath = "static" });
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddRazorPages();
+
 WebApplication app = builder.Build();
 
 
-app.UseDefaultFiles();
 app.UseStaticFiles();
 
-app.Use( async (context,next) =>
-{
-    if(context.Request.Path == "/g")
-         context.Response.Redirect("https://google.com");
-    if (context.Request.Path == "/y")
-        context.Response.Redirect("https://yandex.ru");
-    else await next.Invoke(context);
 
-});
 
-//TEST
+//app.Use(async (context, next) =>
+//{
+//    if (context.Request.Path == "/g")
+//        context.Response.Redirect("https://google.com");
+//    if (context.Request.Path == "/y")
+//        context.Response.Redirect("https://yandex.ru");
+//    else await next.Invoke(context);
 
-app.Run(async (context) =>
-{
-    await context.Response.WriteAsync(context.Request.Path);
-});
+//});
 
+
+//app.Run(async (context) =>
+//{
+//    await context.Response.WriteAsync(context.Request.Path);
+//});
+
+
+app.MapRazorPages();
 
 app.Run();
 
