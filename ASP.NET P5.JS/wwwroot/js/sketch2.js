e237892711e4c4b0a6bdcbed5b864d0e4698da36
MD-KpRk <griferad@gmail.com>
2022-02-06 17:55:27 +0300

    Упростил sketch2. Добавил gif визуализации для скетчей

--- ASP.NET P5.JS/wwwroot/js/sketch2.js
+++ ASP.NET P5.JS/wwwroot/js/sketch2.js
@@ -1,75 +1,45 @@
-var x = 0;
-var y = 0;
-let mode = 'NONE';
+let x = 0;
+let y = 0;
 
 function setup() {
     createCanvas(600, 600);
     textSize(24);
-    background(100);
+    background(120);
+    strokeWeight(3);
+    frameRate(60);
+
+    slider = createSlider(1, 20, 10);
+    slider.position(9, 75);
+
+    colorPicker = createColorPicker('#000000');
+    colorPicker.position(10, 40);
 
     button1 = createButton('Clear');
     button1.position(10, 10);
     button1.mousePressed(clearBackground);
 
-    button2 = createButton('Brush');
-    button2.position(10, 40);
-    button2.mousePressed(setModeBrush);
+    slider.input(() => {
+        strokeWeight(0);
+    });
 
-    button2 = createButton('Rectangle');
-    button2.position(10, 70);
-    button2.mousePressed(setModeRect);
 }
 
 function draw() {
-
-    switch (mode) {
-        case 'Brush':
-            if (mouseIsPressed) {
-                if (x != 0)
-                    line(x, y, mouseX, mouseY);
-                x = mouseX;
-                y = mouseY;
-            } else
-                x = y = 0;
-            break;
-
-        case 'Rectangle':
-            if (mouseIsPressed) {
-                if (x == 0) {
-                    x = mouseX;
-                    y = mouseY;
-                    noStroke();
-                    circle(x, y, 1);
-                    stroke(255);
-                }
-
-            }
-            else {
-                if (x != 0 || y != 0) {
-                    rect(x, y, mouseX - x, mouseY -y);
-                    x = y = 0;
-                }
-            }
-
-            break;
-
-
+    stroke(colorPicker.color());
+    strokeWeight(slider.value());
+    if (mouseIsPressed) {
+        if (x != 0)
+            line(x, y, mouseX, mouseY);
+        x = mouseX;
+        y = mouseY;
     }
-}
-
-function setModeBrush() {
-    x = y = 0
-    mode = 'Brush';
-    stroke(1);
-    strokeWeight(3);
-}
-
-function setModeRect() {
-    x = y = 0
-    mode = 'Rectangle';
-    stroke(256);
+    else
+        x = y = 0;
+    strokeWeight(0);
+    fill(100);
+    rect(0, 0, 150, 100);
 }
 
 function clearBackground() {
-    background(100);
+    background(120);
 }
