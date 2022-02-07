7a849021b13e4afb349ccaf2f34ceca7f5ed9d60
MD-KpRk <griferad@gmail.com>
2022-02-07 17:23:07 +0300

    Изменение sketch3. Убран скетч отражения луча. Вместо него добавлен скетч с ковром Серпинского

--- ASP.NET_P5.JS/wwwroot/js/sketch3.js
+++ ASP.NET_P5.JS/wwwroot/js/sketch3.js
@@ -1,24 +1,38 @@
-let bgcolor = 120;
-
 function setup() {
     createCanvas(600, 600);
-    background(bgcolor);
+    background(120);
+    noFill();
+    frameRate(24);
+    drawFractal();
 }
 
-let x = 500, y1 = 0, y2 = 600;
+let lvl = 4;
+let myscale = 1;
+
 
 function draw() {
-    background(bgcolor);
-    stroke(0);
-    line(x, y1, x, y2);
+    background(120);
+    myscale += 0.05;
+    if (myscale >= 3.0) myscale = 1;
+    drawFractal(myscale);
+}
 
-    let newY = mouseY;
-    let centerY = height / 2;
-    strokeWeight(3);
-    line(0, centerY, x, newY);
+function drawFractal(sc) {
+    drawRect(0, 0, width * sc, height * sc, lvl);
+}
 
-    stroke(255);
-    line(x, newY, 0, newY - (centerY - newY));
-    strokeWeight(1);
+function drawRect(x, y, w, h, level) {
+    let newW = w / 3;
+    let newH = h / 3;
+
+    let arrX = [x, x + newW, x + newW + newW];
+    let arrY = [y, y + newH, y + newH + newH];
+    for (let j = 0; j < 3; j++)
+        for (let i = 0; i < 3; i++)
+            if (j % 2 == 0 || i % 2 == 0) {
+                rect(arrX[i], arrY[j], newW, newH);
+                if (level > 0)
+                    drawRect(arrX[i], arrY[j], newW, newH, level - 1);
+            }
 
 }
